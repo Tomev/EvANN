1f1fbea1080e70bd0f732262a75a297bafca2921
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2019-10-27 20:11:13 +0100

    Log normalizers fix.

--- ObjectiveFunctions/Normalizers/boundedLogNormalizer.cpp
+++ ObjectiveFunctions/Normalizers/boundedLogNormalizer.cpp
@@ -5,6 +5,8 @@
 #include "boundedLogNormalizer.h"
 
 double boundedLogNormalizer::normalize(double const &value) {
-  double normalizedVal = log10(value);
+  double normalizedVal;
+  if(value < 10) normalizedVal = value / 10.0; // Linear under 10
+  else normalizedVal = log10(value);
   return 1.0 - (normalizedVal / limit);
 }
--- ObjectiveFunctions/Normalizers/logNormalizer.cpp
+++ ObjectiveFunctions/Normalizers/logNormalizer.cpp
@@ -2,7 +2,9 @@
 #include <math.h>
 
 double logNormalizer::normalize(double const &value) {
-  double normalizedVal = log10(value);
+  double normalizedVal;
+  if(value < 10) normalizedVal = value / 10.0; // Linear under 10
+  else normalizedVal = log10(value);
   highestError = highestError > normalizedVal ? highestError : normalizedVal;
   return 1.0 - (normalizedVal / highestError);
 }
